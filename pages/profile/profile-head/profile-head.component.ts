3c6c510e35bbec2acde2c2f20049644804f6ab30
aramanenka <arramanenka@gmail.com>
2020-08-24 12:23:34 +0300

    separate component for profile

---
+++ pages/profile/profile-head/profile-head.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from '../../../src/model/user';
+
+@Component({
+    selector: 'app-profile-head',
+    templateUrl: './profile-head.component.html',
+    styleUrls: ['./profile-head.component.scss'],
+})
+export class ProfileHeadComponent implements OnInit {
+
+    @Input() user: User;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
