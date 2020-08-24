163253709e9d7b95861d91f1957e96518570d869
aramanenka <arramanenka@gmail.com>
2020-08-24 12:30:14 +0300

    routing

--- pages/profile/profile-routing.module.ts
+++ pages/profile/profile-routing.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { ProfilePage } from './profile.page';
+import {ProfilePage} from './profile.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ProfilePage
-  }
+    {
+        path: '',
+        component: ProfilePage,
+        children: [
+            {
+                path: ':uid'
+            }
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class ProfilePageRoutingModule {}
+export class ProfilePageRoutingModule {
+}
