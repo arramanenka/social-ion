06b4e0ee35d3c40b277992bf937086ea99533fa7
aramanenka <arramanenka@gmail.com>
2020-08-25 16:15:50 +0300

    close menu on item picked

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../service/user.service';
 import {User} from '../../model/user';
+import {MenuController} from '@ionic/angular';
 
 @Component({
     selector: 'app-main-menu',
@@ -13,13 +14,17 @@ export class MainMenuComponent implements OnInit {
     contentId: string;
     user: User;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private menuController: MenuController) {
     }
 
     ngOnInit() {
         this.userService.querySelf(user => {
             this.user = user;
         });
+    }
+
+    closeMenu() {
+        this.menuController.close().then();
     }
 
 }
