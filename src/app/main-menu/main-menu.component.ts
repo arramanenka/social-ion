2172c18c287e4c27d384d05a32557801a8e9ab4f
aramanenka <arramanenka@gmail.com>
2020-09-10 22:40:09 +0300

    tab placement

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../service/user.service';
 import {User} from '../../model/user';
+import {Platform} from '@ionic/angular';
 
 @Component({
     selector: 'app-main-menu',
@@ -10,8 +11,17 @@ import {User} from '../../model/user';
 export class MainMenuComponent implements OnInit {
 
     user: User;
+    tabPlacement: string;
 
-    constructor(private userService: UserService) {
+    constructor(
+        private userService: UserService,
+        private platform: Platform
+    ) {
+        if (platform.is('desktop')) {
+            this.tabPlacement = 'top';
+        } else {
+            this.tabPlacement = 'bottom';
+        }
     }
 
     ngOnInit() {
