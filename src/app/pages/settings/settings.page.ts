6ba4666e7d9a53f4187b1c01c394b2c6a5fb5c66
aramanenka <arramanenka@gmail.com>
2020-09-19 17:43:35 +0300

    settings page update with all buttons

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IdentityService} from '../../service/identity.service';
+import {UserService} from '../../service/user.service';
+import {User} from '../../../model/user';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+    selector: 'app-settings',
+    templateUrl: './settings.page.html',
+    styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+    user: User;
+    nameEditable = true;
 
-  ngOnInit() {
-  }
+    constructor(
+        private identityService: IdentityService,
+        private userService: UserService
+    ) {
+    }
 
+    ngOnInit() {
+        this.userService.querySelf(u => {
+            this.user = u;
+        });
+    }
+
+    updateName(form) {
+    }
 }
